7dc4b8f0419d789a9102ccf09a1ed07617bc9d5e
Tyler Flint <tylerflint@gmail.com>
2020-01-08 23:20:43 -0500

    Fix silly webpack compile errors

--- services/users-api/get.js
+++ services/users-api/get.js
@@ -1,16 +1,16 @@
 import * as user from '../../lib/user';
 
 export const main = (event, context, callback) => {
-  
+
   // instantiate a user
-  const u = user.getUserById(null, 1)
-  
+  const u = user.getUserById(null, 1);
+
   // prepare a response
   const response = {
     statusCode: 200,
     body: JSON.stringify(u),
-  }
-  
+  };
+
   // send the response
-  callback(null, response)
-}
+  callback(null, response);
+};
